dc1c207f58b11c90d39309c121cadd630fca7393
Chris Howe-Jones <chris.howejones@gmail.com>
2014-09-28 21:08:21 +0100

    Added code to serve up pdf reports from data inserted into a postgres database.

---
+++ src/reporting_example/models/db.clj
@@ -0,0 +1,39 @@
+(ns reporting-example.models.db
+     (:require [clojure.java.jdbc :as sql]))
+
+(def db {:subprotocol "postgresql"
+         :subname "//localhost/reporting"
+         :user "chrishowe-jones"
+         :password ""})
+
+(defn create-employee-table []
+  (sql/create-table :employee
+                    [:name "varchar(50)"]
+                    [:occupation "varchar(50)"]
+                    [:place "varchar(50)"]
+                    [:country "varchar(50)"]))
+
+
+(sql/with-connection
+  db
+  (create-employee-table)
+  (sql/insert-rows
+   :employee
+   ["Albert Einstein", "Engineer", "Ulm", "Germany"]
+   ["Alfred Hitchcook", "Movie Director", "London", "UK"]
+   ["Wernher Von Braun", "Rocket Scientist", "Wyrzysk", "Poland"]
+   ["Sigmund Freud", "Neurologist", "Pribor", "Czech Republic"]
+   ["Mahatma Gandi", "Lawyer", "Gujarat", "India"]
+   ["Sachin Tendulkar", "Cricket Player", "Mumbai", "India"]
+   ["Michael Schumacher", "F1 Racer", "Cologne", "Germany"]))
+
+(defn read-employees []
+  (sql/with-connection db
+    (sql/with-query-results rs
+      ["select * from employee"]
+      (doall rs))))
+
+(read-employees)
+
+(comment sql/with-connection db
+  (sql/drop-table :employee))
